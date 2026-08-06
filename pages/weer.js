4f926e2c69337de6413d0b657cf1c41ddea56fed
Peter Krijgsman <36139454+Peterkrijgsman85@users.noreply.github.com>
2026-08-06 15:59:34 +0200

    Update weer.js

--- pages/weer.js
+++ pages/weer.js
@@ -174,7 +174,7 @@ export const page = {
         display: flex;
         justify-content: center;
         gap: 8px;
-        margin-top: 4px;
+        margin-top: 3px;
       }
 
       .carousel-dot {
